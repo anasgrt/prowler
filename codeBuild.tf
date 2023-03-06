9b0b6f96876ad509008765b3909f4fec0c902e58
anasnedtech <114829917+anasnedtech@users.noreply.github.com>
2023-03-06 14:33:44 +0100

    Reduce permissions of Codebuild Service - Modify AWS Policies

--- codeBuild.tf
+++ codeBuild.tf
@@ -2,8 +2,14 @@ resource "aws_s3_bucket" "prowlerBucket" {
   bucket        = "prowler-kabisa-bucket1985"
   force_destroy = true
 }
+// Codebuild IAM role:
+resource "aws_iam_role" "prowler_codebuild_role" {
+  name = "codebuild_bucket_role"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role_policy.json
+}
 
-data "aws_iam_policy_document" "prowler_codebuild_role" {
+// Ploicies attached to Codebuild IAM role:
+data "aws_iam_policy_document" "codebuild_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -14,16 +20,62 @@ data "aws_iam_policy_document" "prowler_codebuild_role" {
   }
 }
 
-resource "aws_iam_role" "prowler_codebuild_role" {
-  name               = "prowler-codebuild-project-role"
-  assume_role_policy = data.aws_iam_policy_document.prowler_codebuild_role.json
+// Policies for S3 bucket and Cloudwatch logs with attachments to Codebuild Iam role:
+resource "aws_iam_policy" "codebuild_bucket_policy" {
+  name        = "codebuild_bucket_policy"
+  description = "Policy that allows CodeBuild to put objects in S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.prowlerBucket.arn}",
+          "${aws_s3_bucket.prowlerBucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_deploy" {
+resource "aws_iam_role_policy_attachment" "codebuild_bucket_policy_attachment" {
+  policy_arn = aws_iam_policy.codebuild_bucket_policy.arn
   role       = aws_iam_role.prowler_codebuild_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
+resource "aws_iam_policy" "codebuild_log_policy" {
+  name        = "codebuild_log_policy"
+  description = "Policy that allows CodeBuild to create CloudWatch Logs log streams"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_log_policy_attachment" {
+  policy_arn = aws_iam_policy.codebuild_log_policy.arn
+  role       = aws_iam_role.prowler_codebuild_role.name
+}
+
+// Codebuild infrastructure:
 resource "aws_codebuild_project" "prowler_codebuild_project" {
   name         = "prowler-codebuild-project"
   description  = "Prowler Codebuild Project"
